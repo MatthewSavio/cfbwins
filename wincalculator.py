32c48757fa89f91ee2a867f611c6b7e07c249c04
Matt Savio Nicholas <mattnich@umich.edu>
2016-09-21 20:21:39 -0400

    Create wincalculator.py
    Commenting notes for cfb project

---
+++ wincalculator.py
@@ -0,0 +1,15 @@
+# (notes)
+# take cfbteam as inputs
+# take cfbuntil2010wins as input
+# using BeautifulSoup and tweepy and math
+# grab wins since 2011 from "http://www.sports-reference.com/cfb/schools/" + cfbteam + "/"
+# confirm that latest year = current year
+# add up wins 
+# add up losses
+# add up ties (no adding needed)
+# # calculate winning % = ( total wins + total losses / 2 ) / num all games 
+# find next 100 win milestone (int(math.ceil(x/100.0))*100)
+# winsleft to milestone = milestone - totalwins
+# return winsleft to milestone
+# predict when will pass next 100 win milestone (= winsleft / winning %) = num-games-to-play
+
